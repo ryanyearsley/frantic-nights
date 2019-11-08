11b785c75ee6a1182e29a12598f982944e567f41
skiddmcmarx <41240083+skiddmcmarx@users.noreply.github.com>
2019-11-08 16:34:28 -0500

    Added Functional Menu

--- frantic-nights/Assets/scripts/manager/MenuManager.cs
+++ frantic-nights/Assets/scripts/manager/MenuManager.cs
@@ -4,10 +4,71 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
+
+public enum MenuState
+{
+    main,
+    levelSelect
+}
+
+
 public class MenuManager : MonoBehaviour
 {
+    private MenuState currentMenuState;
+    private GameObject canvas;
     private GameObject mainPanel;
     private GameObject levelSelectPanel;
-    private GameObject optionsPanel;
-    private GameObject thanksForPlayingPanel;
+    //private GameObject optionsPanel;
+    //private GameObject thanksForPlayingPanel;
+
+    private void Start()
+    {
+        canvas = GameObject.Find("Canvas");
+        mainPanel = canvas.transform.Find("mainPanel").gameObject;
+        levelSelectPanel = canvas.transform.Find("levelSelectPanel").gameObject;
+
+    }
+
+    //Menu Panel Button Events
+    public void quitGame()
+    {
+        Application.Quit();
+    }
+    public void setPanelMain()
+    {
+        setPanel(MenuState.main);
+    }
+    public void setPanelLevelSelect()
+    {
+        setPanel(MenuState.levelSelect);
+    }
+
+    //Load Level Button Events
+    public void loadLevelFreeRoam()
+    {
+        SceneManager.LoadScene("GroundVehicleDemo 2");
+    }
+
+
+
+    public void setPanel(MenuState menuState)
+    {
+        currentMenuState = menuState;
+        switch (menuState)
+        {
+            case MenuState.main:
+                mainPanel.SetActive(true);
+                levelSelectPanel.SetActive(false);
+                break;
+            case MenuState.levelSelect:
+                mainPanel.SetActive(false);
+                levelSelectPanel.SetActive(true);
+                break;
+            default:
+                mainPanel.SetActive(true);
+                levelSelectPanel.SetActive(false);
+                break;
+        } 
+                
+    }
 }
