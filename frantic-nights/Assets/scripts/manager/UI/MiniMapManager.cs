806a8bdcd67c686ae6447b4309c0e78c106f417e
skiddmcmarx <41240083+skiddmcmarx@users.noreply.github.com>
2020-07-05 20:27:53 -0400

    Revert "Transparent + Unlit Mini-Map"
    This reverts commit 9813eb4c6a839f6377acdf6abe8de25ca7d22f24.

--- frantic-nights/Assets/scripts/manager/UI/MiniMapManager.cs
+++ frantic-nights/Assets/scripts/manager/UI/MiniMapManager.cs
@@ -1,35 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class MiniMapManager : MonoBehaviour
 {
-
-    public GameObject playerIndicator;
-
-    //private RawImage miniMap;
-    //private Camera miniMapCamera;
-    private Transform cameraTrans;
-    private Transform playerTrans;
-    private Transform playerIndicatorTrans;
-
-
-
     // Start is called before the first frame update
     void Start()
     {
-        cameraTrans = this.transform;
-        playerTrans = FindObjectOfType<PlayerController>().transform;
-        playerIndicatorTrans = Instantiate(playerIndicator).transform;
+        
     }
 
     // Update is called once per frame
-    void LateUpdate()
+    void Update()
     {
-        cameraTrans.position = new Vector3(playerTrans.position.x, playerTrans.position.y + 20, playerTrans.position.z);
-
-
-        playerIndicatorTrans.SetPositionAndRotation(playerTrans.position, playerTrans.rotation);
+        
     }
 }
